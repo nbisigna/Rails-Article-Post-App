f5a8ecf1f94a31ad19d28f4799ca3408be17348f
Nick <nbisigna@gmail.com>
2017-10-14 18:34:02 -0400

    another commit

---
+++ app/assets/javascripts/comments.coffee
@@ -0,0 +1,8 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on "turbolinks:load", ->
+  $(".comments-link").click (event) ->
+    event.preventDefault()
+    $('#comments-section').fadeToggle()
+    $('#comment_body').focus()
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013230903) do
+ActiveRecord::Schema.define(version: 20171014191743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_comments_on_ancestry"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
